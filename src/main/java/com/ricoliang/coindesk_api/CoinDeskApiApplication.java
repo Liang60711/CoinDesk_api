f2ee546c565b5399b6ba0812573428adeca6732b
Liang <liang60711@gmail.com>
2022-07-13 22:00:24 +0800

    init commit

---
+++ src/main/java/com/ricoliang/coindesk_api/CoinDeskApiApplication.java
@@ -0,0 +1,13 @@
+package com.ricoliang.coindesk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinDeskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinDeskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ricoliang/coindesk_api/CoinDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricoliang.coindesk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinDeskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
